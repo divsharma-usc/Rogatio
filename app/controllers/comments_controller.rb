2986d0f23a77c98dd7a8309cf4bb82d15daf3e12
Divyansh Sharma <divyansh9595@gmail.com>
2017-07-26 00:43:15 +0530

    com improved

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,22 +2,26 @@ class CommentsController < ApplicationController
   before_action :authenticate_user!
   def docomment
   	@answerid=params[:answerid].to_i
-    @comments=Comment.where(answer_id: params[:answerid].to_i).limit(2)
+    @comments=Comment.where(answer_id: params[:answerid].to_i).order(updated_at: :DESC).limit(2)
     @more=Comment.where(answer_id: params[:answerid].to_i).length>2
   	respond_to do |format|
   		format.js{}
     end
   end
   def createcomment
-  	  comment=Comment.new
-  	  comment.content=params[:comment]
-  	  comment.user=current_user
-  	  comment.answer_id=params["answer_id"].to_i
-  	  comment.save!
+  	  @comment=Comment.new
+  	  @comment.content=params[:comment]
+  	  @comment.user=current_user
+  	  @comment.answer_id=params["answer_id"].to_i
+  	  @comment.save!
+      @answerid=params["answer_id"].to_i
+      respond_to do |format|
+        format.js{}
+      end
   end
   def loadmore
       @offset=params["offset"].to_i
-      @comments=Comment.where(answer_id: params["answerid"].to_i).offset(@offset).limit(10);
+      @comments=Comment.where(answer_id: params["answerid"].to_i).order(updated_at: :DESC).offset(@offset).limit(10);
       @answerid=params["answerid"].to_i
       @length=Comment.where(answer_id: @answerid).length
       if @offset+10 >= @length
